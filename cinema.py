65842dcc993778243c21115c929a5153ed43f0ff
bjornlecis <bjorn.lecis@gmail.com>
2021-11-14 14:46:16 +0100

    begin situatie

---
+++ cinema.py
@@ -0,0 +1,18 @@
+leeftijd = 0
+prijs = 0
+film3d = "nee"
+
+
+
+if(leeftijd < 12):
+    prijs = 3
+elif(leeftijd < 17):
+    prijs = 4
+else:
+    prijs = 5
+if(film3d = "ja"):
+    prijs = prijs +2
+else
+    prijs = prijs + 0
+
+print("persoon 1 betaald:",kostprijs)
